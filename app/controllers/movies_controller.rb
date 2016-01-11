8151833caca37762ecfe43f85eaf5eb3fdeaadc6
Khangal <khangalkola@gmail.com>
2016-01-11 12:29:32 +0000

    hw2 part1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @orderBy = params[:orderBy] || "id"
+    @movies = Movie.order(@orderBy + " asc")
   end
 
   def new
